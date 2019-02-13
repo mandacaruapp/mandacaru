377c6d461d800b7efe03acbd22a353913fdc75d4
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2019-02-12 18:34:04 -0800

    Temporarely hide menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,11 +3,11 @@ import "./NavBar.css"
 
 export class NavBar extends React.Component{
     render(){
-        
+
         return (
             <ul>
-                <li key='1'><a href="x">About us</a></li>
-                <li key='2'><a href="x">Contact</a></li>
+                {/* <li key='1'><a href="x">About us</a></li>
+                <li key='2'><a href="x">Contact</a></li> */}
             </ul>
         )
     }
